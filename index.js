a3c3d193b393e43797d3e84ab5319622060ee975
Aditya Biswal <95116291+adityabiswal2147207@users.noreply.github.com>
2022-12-09 22:31:35 +0530

    updated

--- index.js
+++ index.js
@@ -1,45 +1,90 @@
-//importing the modules
+// including the packages
 const express = require('express');
-var ejs = require('ejs');
+const ejs = require('ejs');
+const path = require('path');
 const bodyParser = require('body-parser');
 const mysql = require('mysql');
 
 
-//this is the demo connection dont use this one
-const db = mysql.createConnection({
-    host:"",
-    port:"",
-    user:"",
-    password:"",
-    database:""
-});
-
-
-
-
-//calling the function into the variable name of tyoe constant
+//making the variable out of the package function
 const app = express();
 
-//setting up the engine and serving the folder 
-app.use(express.static('public'));
+//creating a database connection
+const db = mysql.createConnection({
+    host:"database-1.ceedmie6y0m6.us-east-1.rds.amazonaws.com",
+    port:"3306",
+    user:"admin",
+    password:"aditya2110",
+    database:"covidbase"
+});
+
+db.connect(function(error){
+    if(!!error) console.log(error);
+    else console.log('Database Connected');
+});
+
+//setting views file
+app.set('views',path.join(__dirname,'views'));
+
+//set view engine
 app.set('view engine','ejs');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
 
-app.listen(8080);
-app.use(bodyParser.urlencoded({extended:true}));
+//creating an arrow function
+app.get('/',(req,res)=>{
+    let sql = "SELECT * FROM new_table";
+    let query = db.query(sql,(err,rows)=>{
+        if(err) throw err;
+        res.render('index',{
+            title : 'Welcome To AWS TODO List',
+            users : rows
+        });
+    });
+});
 
-app.get('/',function(req,res){
+app.get('/add',(req,res)=>{
+    res.render('add',{
+        title : 'Welcome To AWS TODO List'
+    })
+});
 
-    const db = mysql.createConnection({
-        host:"database-1.ceedmie6y0m6.us-east-1.rds.amazonaws.com",
-        port:"3306",
-        user:"admin",
-        password:"aditya2110",
-        database:"covidbase"
+app.post('/save',(req,res)=>{
+    let data = {todo: req.body.todo, priority: req.body.priority, description: req.body.description, status: req.body.status};
+    let sql = "INSERT INTO new_table SET?";
+    let query = db.query(sql,data,(err,results)=>{
+        if(err) throw err;
+        res.redirect('/');
+    });
+});
+
+app.get('/edit/:userId',(req,res)=>{
+    const userId = req.params.userId;
+    let sql = `SELECT * FROM new_table where id = ${userId}`;
+    let query = db.query(sql,(err,result)=>{
+        if(err) throw err;
+        res.render('edit',{
+            title: 'Welcome To AWS TODO List',
+            users: result[0]
+        });
+    });
+});
+
+app.post('/update',(req,res)=>{
+    const userId = req.params.id;
+
+});
+
+app.get('/delete/:userId',(req,res)=>{
+    const userId = req.params.userId;
+    let sql = `DELETE FROM new_table where id = ${userId}`;
+    let query = db.query(sql,(err,result)=>{
+        if(err) throw err;
+        res.redirect('/');
+        });
     });
 
-    db.query("SELECT * FROM info",(err,result)=>{
-
-        res.render('pages/index',{result:result});
-    })
-    
+//now listening to the server
+app.listen(8080,()=>{
+    console.log("Server is now running on port 8080");
 });
